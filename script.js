08e33762741d502eacba825d296e625671bffd37
Ari-keys <142097462+Ari-keys@users.noreply.github.com>
2023-08-12 06:22:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Create a Leaflet map
-var map = L.map('map').setView([51.505, -0.09], 13);
+/* eslint-disable no-undef */
+/**
+ * Simple map
+ */
 
-// Add a tile layer (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// config map
+let config = {
+  minZoom: 7,
+  maxZoom: 18,
+};
+// magnification with which the map will start
+const zoom = 18;
+// co-ordinates
+const lat = 52.22977;
+const lng = 21.01178;
+
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-
-// Add a marker to the map
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A marker on the map!')
-    .openPopup();
